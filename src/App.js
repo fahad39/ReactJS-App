8de89e676e44d0ac382e1496cc32eb785f684b37
fahad <fahad.hussain@thehexaa.com>
2022-06-09 16:38:00 +0500

    added api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { create } from "apisauce";
+
+import SearchIcon from "./search.svg";
+
+//  2671c405
+
+const API_URL = "http://www.omdbapi.com/";
+
+const api = create({
+  baseURL: API_URL,
+  headers: { Accept: "application/json" },
+});
 
 function App() {
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  const searchMovies = async (searchTerm) => {
+    const response = await api.get("/", {
+      apikey: "2671c405",
+      s: searchTerm,
+    });
+    console.log(response.data.Search);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Land</h1>
     </div>
   );
 }
